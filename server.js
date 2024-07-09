b2b4fb8a731ec1a076186f7e8ac8ccc9bd1958bf
Jin Park <jinpark1dev@gmail.com>
2024-07-09 13:41:00 -0700

    added ruru graphiql

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var { graphql, buildSchema } = require("graphql");
+var express = require("express");
+var { createHandler } = require("graphql-http/lib/use/express");
+var { ruruHTML } = require("ruru/server");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -14,7 +17,7 @@ var rootValue = {
     return "Hello world!";
   },
   age() {
-    return 25;
+    return 27;
   },
 };
 
@@ -26,3 +29,25 @@ graphql({
 }).then((response) => {
   console.log(response);
 });
+
+const app = express();
+
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: rootValue,
+  })
+);
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
+
+app.listen(4000);
+console.log(`
+	Api running on: http://localhost:4000)
+	Test: http://localhost:4000/graphql?query={hello,age}
+	`);

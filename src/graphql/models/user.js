41307e0e13d79bfd99f863d5ac72d8f66a3c77f9
Jin Park <jinpark1dev@gmail.com>
2024-07-09 23:03:05 -0700

    user comments connection

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -24,6 +24,8 @@ export const typeDef = /* GraphQL */ `
     id: ID!
     name: String
     email: String
+
+    comments: [Comment]
   }
 `;
 
@@ -61,6 +63,9 @@ export const resolvers = {
     id: ({ id, _id }) => _id || id,
     name: (obj) => {
       return obj.name.trim().toUpperCase();
+    },
+    comments: ({ email }, args, { mongo }) => {
+      return mongo.comments.find({ email }).limit(20).toArray();
     },
   },
 };
